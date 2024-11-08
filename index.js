879a15716b48ab2270971398c2dac7cc68dff8d2
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-11-08 23:23:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://festusekuruemu:DZXvqk6TBPp5lbiJ@airrrr.idao5.mongodb.net/?retryWrites=true&w=majority&appName=airrrr');
+
+const sensorSchema = new mongoose.Schema({
+  temperature: Number,
+  humidity: Number,
+  co_level: Number,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const SensorData = mongoose.model('SensorData', sensorSchema);
+
+app.use(bodyParser.json());
+
+app.post('/api/sensordata', async (req, res) => {
+  try {
+    const { temperature, humidity, co_level } = req.body;
+
+    // Create a new record in the database
+    const newData = new SensorData({ temperature, humidity, co_level });
+    await newData.save();
+
+    res.status(200).json({ message: 'Data saved successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error saving data', error });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
